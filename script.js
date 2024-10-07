a62275e981ee7b1cc269cc13d3ae139aa82a06f1
NickW16 <171855220+NickW16@users.noreply.github.com>
2024-10-07 12:04:49 -0300

    Add style

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ let divBox = document.getElementById("box-container");
 
 for (let i = 0; i < 256; i++) {
     let squareDiv = document.createElement('div');
+    squareDiv.addEventListener('mouseover', function() {
+        squareDiv.style.backgroundColor = "black";
+    });
     divBox.appendChild(squareDiv);
 }
+
